8d18f4cd04675185e163fda7c8b19181726e5eb9
Xtarget <xtarget.plus@gmail.com>
2023-11-16 12:13:45 +0300

    NEW: tests

---
+++ FigureLibrary.Tests/FigureWithSideTest.cs
@@ -0,0 +1,63 @@
+﻿using FigureLibrary.Figures.Concrete;
+using Xunit;
+
+namespace FigureLibrary.Tests;
+
+public class FigureWithSideTest
+{
+    [Fact]
+    public void FigureTestSquare()
+    {
+        // Arrange
+        var figure = new FigureWithSide(FigureWithSideType.Square);
+        double[] arr = { 23 };
+
+        // Act
+        var result = figure.CalculateArea(arr);
+
+        // Fact
+        Assert.Equal(529, result);
+    }
+
+    [Fact]
+    public void FigureTestTriangle()
+    {
+        // Arrange
+        var figure = new FigureWithSide(FigureWithSideType.Triangle);
+        double[] arr = { 23, 14, 11 };
+
+        // Act
+        var result = figure.CalculateArea(arr);
+
+        // Fact
+        Assert.Equal(55.85696, result);
+    }
+
+    [Fact]
+    public void FigureTestIsoscelesTriangle()
+    {
+        // Arrange
+        var figure = new FigureWithSide(FigureWithSideType.Triangle);
+        double[] arr = { 23, 14, 23 };
+
+        // Act
+        var result = figure.CalculateArea(arr);
+
+        // Fact
+        Assert.Equal(153.362316, result);
+    }
+
+    [Fact]
+    public void FigureTestEquilateralTriangle()
+    {
+        // Arrange
+        var figure = new FigureWithSide(FigureWithSideType.Triangle);
+        double[] arr = { 23, 23, 23 };
+
+        // Act
+        var result = figure.CalculateArea(arr);
+
+        // Fact
+        Assert.Equal(229.063719, result);
+    }
+}
---
+++ FigureLibrary.Tests/FigureWithoutSideTest.cs
@@ -0,0 +1,20 @@
+﻿using FigureLibrary.Figures.Concrete;
+using Xunit;
+
+namespace FigureLibrary.Tests;
+
+public class FigureWithoutSideTest
+{
+    [Fact]
+    public void FigureTestCircle()
+    {
+        // Arrange
+        var figure = new FigureWithoutSide(FigureWithoutSideType.Circle);
+
+        // Act
+        var result = figure.CalculateArea(23);
+
+        // Fact
+        Assert.Equal(1661.902697, result);
+    }
+}
